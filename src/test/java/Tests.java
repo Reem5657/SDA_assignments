1142b7cdbda0d5a45f606b23b78bc94e7b9e97fa
reem <reem.oo.m@hotmail.com>
2024-03-20 01:13:32 +0300

    Task1week6

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,10 +1,11 @@
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.time.Duration;
+
 
 public abstract class Tests {
 
